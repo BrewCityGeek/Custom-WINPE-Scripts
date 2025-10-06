affa3e91c4296260eb803e9ae198af9bdf4dc697
BrewCityGeek <brewcitygeek@gmail.com>
2025-10-06 15:32:32 -0500

    Update mount-image.cmd

--- DISM/mount-image.cmd
+++ DISM/mount-image.cmd
@@ -1,3 +1,4 @@
-REM Replace C:\Winpe_amd64 with the path of your choosing
+@echo off
+REM Replace C:\images with the path of your choosing
 
 dism /mount-image /imagefile:C:\images\boot.wim /index:1 /mountdir:C:\images\mount /checkintegrity
